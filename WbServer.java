5d33ebe3808a1e3fffa12f6f7ff3662ec010e060
akshatha <akshatha_b@persistent.com>
2021-09-11 16:29:04 +0530

    This is first commit

---
+++ WbServer.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class WbServer {
+    public static void main(String[] args)throws Exception {
+		ServerSocket ss;
+        Thread t;
+        Socket sc;
+        try
+        {
+        ss=new ServerSocket(9999);
+        System.out.println("Server is Listening at" + InetAddress.getLocalHost().getHostAddress()+"at port"+ss.getLocalPort());
+        
+        while(true)
+        {
+            sc=ss.accept();
+            
+            t=new WbThread(sc);   
+        }
+        }
+        catch(Exception e)
+		{
+		System.out.println(e);
+		}
+    }
+    
+}
